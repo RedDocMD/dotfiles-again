15c10ededd81916acf5252ce17d221ac38d4c672
Deep Majumder <deep.majumder2019@gmail.com>
2022-06-22 11:36:05 +0530

    Wezterm config to jump tabs

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,15 +1,50 @@
 local wezterm = require 'wezterm';
+local table = require 'table';
+
+local tmux_keys = {
+	{key="c", mods="LEADER", action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
+	{key="n", mods="LEADER", action=wezterm.action{ActivateTabRelative=1}},
+	{key="p", mods="LEADER", action=wezterm.action{ActivateTabRelative=-1}},
+	{key="&", mods="LEADER|SHIFT", action=wezterm.action{CloseCurrentTab={confirm=true}}},
+	{key="a", mods="LEADER|CTRL", action=wezterm.action{SendString="\x01"}},
+	{key="-", mods="LEADER", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+	{key="\\", mods="LEADER", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+	{key="j", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Down"}},
+	{key="k", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Up"}},
+	{key="l", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Right"}},
+	{key="h", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Left"}},
+}
+
+local keys = {}
+for _, key in ipairs(tmux_keys) do
+	table.insert(keys, key)
+end
+for i = 1, 9 do
+	table.insert(keys,
+	  {key=tostring(i), mods="LEADER", action=wezterm.action{ActivateTab=i-1}})
+end
 
 return {
 	color_scheme = "Gruvbox Dark",
 	font = wezterm.font("Iosevka Term"),
 	leader = {key="a", mods="CTRL", timeout_milliseconds=400},
-	keys = {
-		{key="c", mods="LEADER", action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
-		{key="n", mods="LEADER", action=wezterm.action{ActivateTabRelative=1}},
-		{key="p", mods="LEADER", action=wezterm.action{ActivateTabRelative=-1}},
-		{key="&", mods="LEADER|SHIFT", action=wezterm.action{CloseCurrentTab={confirm=true}}},
-		{key="a", mods="LEADER|CTRL", action=wezterm.action{SendString="\x01"}},
-	},
+	keys = keys,
 	scrollback_lines = 100000,
+	ssh_domains = {
+		{name="collosus", remote_address="192.168.29.47", username="dknite"}
+	},
+	hyperlink_rules = {
+		{
+			regex = [[\b\w+://[\w.-]+\.[a-z]{2,15}\S*\b]],
+			format = "$0",
+		},
+		{
+			regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
+			format = "$0",
+		},
+		{
+			regex = [[\bhttp://localhost:\d{1,4}\b]],
+			format = "$0",
+		}
+	},
 }
